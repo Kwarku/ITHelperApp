8a64416ade592ec4185311fab316234799d94cae
Paweł Kuklicz <p.kuklicz@gmail.com>
2018-06-16 18:18:59 +0200

    start project

---
+++ README.md
@@ -0,0 +1,21 @@
+##IT helper application.
+
+You have some problems with your devices and tou don't know how you can fix this problem. 
+
+Now we have **solution** for **You**!
+
+
+Open this application and add your problem to list. Some nice student can help.
+
+
+
+
+*Technology used:*
+
+- JavaEE
+- Hibernate
+- HTML
+- CSS
+- JS
+
+
---
+++ src/LoginServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+
+        if (login.equals("pawel") && password.equals("123")) {
+            resp.getWriter().write("Jesteś zalogowany");
+        }else {
+            resp.getWriter().write("Błąd w logowaniu");
+
+        }
+
+    }
+}
